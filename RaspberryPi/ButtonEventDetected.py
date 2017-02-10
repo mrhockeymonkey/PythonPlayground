7ba9fda9f6b88be993cc6febd4015b5b3b9b70ce
mrhockeymonkey <s.a.matthews@live.co.uk>
2017-02-10 21:09:20 +0000

    adding buttoneventdetected.py and comments

---
+++ RaspberryPi/ButtonEventDetected.py
@@ -0,0 +1,22 @@
+"""
+This script will check for events every 3 seconds
+Even though the gap is longer than in ButtonPolling.py, becuase we
+are using events we should not see any inputs missed
+"""
+
+import RPi.GPIO as GPIO
+import time
+
+# setup pin 7 as input with a default value of ON
+pin = 7
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# create a new listener for events (Events are RISING or FALLING or BOTH)
+GPIO.add_event_detect(pin, GPIO.FALLING)
+
+while True:
+	if (GPIO.event_detected(pin)):
+		print('Button pressed')
+
+	time.sleep(3)
--- RaspberryPi/ButtonPolling.py
+++ RaspberryPi/ButtonPolling.py
@@ -1,12 +1,17 @@
+"""
+polls input pin every half second to see if button is pressed
+if the button is pressed and released quickly the input is not captured
+"""
+
 import RPi.GPIO as GPIO
 import time
 
-#Setup pin 7 as input with a default value of ON
+# setup pin 7 as input with a default value of ON
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-	# if the button is pressed, the circuit connected, then pin 7 will beocme LOW
+	# if the button is pressed, the circuit connected, then pin 7 will beocme LOW or OFF
 	if(GPIO.input(7)) == False:
 		print('Button is pressed')
 	else:
