ea5bf469bbff32c386540b66d18b9ef8102f47c9
mrhockeymonkey <s.a.matthews@live.co.uk>
2017-02-10 18:36:07 +0000

    using pull up resistor

--- RaspberryPi/ButtonPolling.py
+++ RaspberryPi/ButtonPolling.py
@@ -1,14 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
-#Setup pin 7 as input with a default value of OFF
+#Setup pin 7 as input with a default value of ON
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-	if(GPIO.input(7)):
+	# if the button is pressed, the circuit connected, then pin 7 will beocme LOW
+	if(GPIO.input(7)) == False:
 		print('Button is pressed')
-	else:
+	elif == True:
 		print('Button is not pressed')
 	
-	time.sleep(1)
+	time.sleep(0.5)
